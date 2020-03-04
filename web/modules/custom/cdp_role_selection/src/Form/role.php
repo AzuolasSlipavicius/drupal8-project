c963f36f5b98ba8d3f00df3f30f2a8a0c17764a5
Azuolas <azuolas.slipavicius@gmail.com>
2020-03-04 14:46:16 +0200

    Added custom module cdp_role_selection

--- web/modules/custom/cdp_role_selection/src/Form/role.php
+++ web/modules/custom/cdp_role_selection/src/Form/role.php
@@ -28,10 +28,35 @@ class role extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['example'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('cdp_role_selection.settings')->get('example'),
+
+    $config = \Drupal::service('config.factory')->getEditable('cdp_role_selection.settings');
+
+    //Returns selected roles from settings
+    $configRoles =[];
+    foreach ($config->get('roles') as $role) {
+      if ($role !== 0 ) {
+        $configRoles[]=$role;
+      }
+    }
+
+    //Returns role ID
+    $rolesBundle =\Drupal::entityTypeManager()->getStorage('user_role')->loadMultiple();
+    $rolesID=[];
+    foreach ($rolesBundle as $role =>$value) {
+//      if ($role !== 'anonymous' && $role !== 'authenticated' && $role !== 'administrator')
+      $rolesID[] = $role;
+    }
+    $form['role_selection'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('General Configurations'),
+      '#description' => $this->t('Select roles to be displayed in user registration form.'),
+      '#open' => TRUE,
+    ];
+    $form['role_selection']['roles'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Select role'),
+      '#options' => $rolesID,
+      '#default_value' => $configRoles,
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -40,9 +65,6 @@ class role extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
-    }
     parent::validateForm($form, $form_state);
   }
 
@@ -51,7 +73,7 @@ class role extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('cdp_role_selection.settings')
-      ->set('example', $form_state->getValue('example'))
+      ->set('roles', $form_state->getValue('roles'))
       ->save();
     parent::submitForm($form, $form_state);
   }
