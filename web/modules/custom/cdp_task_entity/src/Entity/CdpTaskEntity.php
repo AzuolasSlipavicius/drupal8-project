4d38be0881b0d3c40522009c9c5349c117d7e23d
Azuolas <azuolas.slipavicius@gmail.com>
2020-03-10 14:01:44 +0200

    custom module cdp_task_entity

--- web/modules/custom/cdp_task_entity/src/Entity/CdpTaskEntity.php
+++ web/modules/custom/cdp_task_entity/src/Entity/CdpTaskEntity.php
@@ -143,103 +143,103 @@ class CdpTaskEntity extends ContentEntityBase implements CdpTaskEntityInterface 
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
 
     $fields = parent::baseFieldDefinitions($entity_type);
+//
+//    $fields['title'] = BaseFieldDefinition::create('string')
+//
+//      ->setLabel(t('Title'))
+//      ->setDescription(t('The title of the cdp_task_entity entity.'))
+//      ->setRequired(TRUE)
+//      ->setSetting('max_length', 255)
+//      ->setDisplayOptions('form', [
+//        'type' => 'string_textfield',
+//        'weight' => -5,
+//      ])
+//      ->setDisplayConfigurable('form', TRUE)
+//      ->setDisplayOptions('view', [
+//        'label' => 'hidden',
+//        'type' => 'string',
+//        'weight' => -5,
+//      ])
+//      ->setDisplayConfigurable('view', TRUE);
+//
+//    $fields['description'] = BaseFieldDefinition::create('text_long')
+//      ->setLabel(t('Description'))
+//      ->setDescription(t('A description of the cdp_task_entity.'))
+//      ->setDisplayOptions('form', [
+//        'type' => 'text_textarea',
+//        'weight' => 10,
+//      ])
+//      ->setDisplayConfigurable('form', TRUE)
+//      ->setDisplayOptions('view', [
+//        'type' => 'text_default',
+//        'label' => 'above',
+//        'weight' => 10,
+//      ])
+//      ->setDisplayConfigurable('view', TRUE);
 
-    $fields['title'] = BaseFieldDefinition::create('string')
-
-      ->setLabel(t('Title'))
-      ->setDescription(t('The title of the cdp_task_entity entity.'))
-      ->setRequired(TRUE)
-      ->setSetting('max_length', 255)
-      ->setDisplayOptions('form', [
-        'type' => 'string_textfield',
-        'weight' => -5,
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayOptions('view', [
-        'label' => 'hidden',
-        'type' => 'string',
-        'weight' => -5,
-      ])
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['description'] = BaseFieldDefinition::create('text_long')
-      ->setLabel(t('Description'))
-      ->setDescription(t('A description of the cdp_task_entity.'))
-      ->setDisplayOptions('form', [
-        'type' => 'text_textarea',
-        'weight' => 10,
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayOptions('view', [
-        'type' => 'text_default',
-        'label' => 'above',
-        'weight' => 10,
-      ])
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['status'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Status'))
-      ->setDescription(t('A boolean indicating whether the cdp_task_entity is enabled.'))
-      ->setDefaultValue(TRUE)
-      ->setSetting('on_label', 'Enabled')
-      ->setDisplayOptions('form', [
-        'type' => 'boolean_checkbox',
-        'settings' => [
-          'display_label' => FALSE,
-        ],
-        'weight' => 0,
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayOptions('view', [
-        'type' => 'boolean',
-        'label' => 'above',
-        'weight' => 0,
-        'settings' => [
-          'format' => 'enabled-disabled',
-        ],
-      ])
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
-
-      ->setLabel(t('Author'))
-      ->setDescription(t('The user ID of the cdp_task_entity author.'))
-      ->setSetting('target_type', 'user')
-      ->setDisplayOptions('form', [
-        'type' => 'entity_reference_autocomplete',
-        'settings' => [
-          'match_operator' => 'CONTAINS',
-          'size' => 60,
-          'placeholder' => '',
-        ],
-        'weight' => 15,
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayOptions('view', [
-        'label' => 'above',
-        'type' => 'author',
-        'weight' => 15,
-      ])
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['created'] = BaseFieldDefinition::create('created')
-      ->setLabel(t('Authored on'))
-      ->setDescription(t('The time that the cdp_task_entity was created.'))
-      ->setDisplayOptions('view', [
-        'label' => 'above',
-        'type' => 'timestamp',
-        'weight' => 20,
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayOptions('form', [
-        'type' => 'datetime_timestamp',
-        'weight' => 20,
-      ])
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['changed'] = BaseFieldDefinition::create('changed')
-      ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the cdp_task_entity was last edited.'));
+//    $fields['status'] = BaseFieldDefinition::create('boolean')
+//      ->setLabel(t('Status'))
+//      ->setDescription(t('A boolean indicating whether the cdp_task_entity is enabled.'))
+//      ->setDefaultValue(TRUE)
+//      ->setSetting('on_label', 'Enabled')
+//      ->setDisplayOptions('form', [
+//        'type' => 'boolean_checkbox',
+//        'settings' => [
+//          'display_label' => FALSE,
+//        ],
+//        'weight' => 0,
+//      ])
+//      ->setDisplayConfigurable('form', TRUE)
+//      ->setDisplayOptions('view', [
+//        'type' => 'boolean',
+//        'label' => 'above',
+//        'weight' => 0,
+//        'settings' => [
+//          'format' => 'enabled-disabled',
+//        ],
+//      ])
+//      ->setDisplayConfigurable('view', TRUE);
+//
+//    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+//
+//      ->setLabel(t('Author'))
+//      ->setDescription(t('The user ID of the cdp_task_entity author.'))
+//      ->setSetting('target_type', 'user')
+//      ->setDisplayOptions('form', [
+//        'type' => 'entity_reference_autocomplete',
+//        'settings' => [
+//          'match_operator' => 'CONTAINS',
+//          'size' => 60,
+//          'placeholder' => '',
+//        ],
+//        'weight' => 15,
+//      ])
+//      ->setDisplayConfigurable('form', TRUE)
+//      ->setDisplayOptions('view', [
+//        'label' => 'above',
+//        'type' => 'author',
+//        'weight' => 15,
+//      ])
+//      ->setDisplayConfigurable('view', TRUE);
+//
+//    $fields['created'] = BaseFieldDefinition::create('created')
+//      ->setLabel(t('Authored on'))
+//      ->setDescription(t('The time that the cdp_task_entity was created.'))
+//      ->setDisplayOptions('view', [
+//        'label' => 'above',
+//        'type' => 'timestamp',
+//        'weight' => 20,
+//      ])
+//      ->setDisplayConfigurable('form', TRUE)
+//      ->setDisplayOptions('form', [
+//        'type' => 'datetime_timestamp',
+//        'weight' => 20,
+//      ])
+//      ->setDisplayConfigurable('view', TRUE);
+//
+//    $fields['changed'] = BaseFieldDefinition::create('changed')
+//      ->setLabel(t('Changed'))
+//      ->setDescription(t('The time that the cdp_task_entity was last edited.'));
 
     return $fields;
   }
