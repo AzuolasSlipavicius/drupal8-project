62ae22d99d1bc4fdb684f419eda343d4be226268
Azuolas <azuolas.slipavicius@gmail.com>
2020-03-10 18:46:38 +0200

    custom module cdp_task_entity

--- web/modules/custom/cdp_task_entity/src/Form/CdpTaskEntityForm.php
+++ web/modules/custom/cdp_task_entity/src/Form/CdpTaskEntityForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Form controller for the cdp_task_entity entity edit forms.
+ * Form controller for the cdp task entity entity edit forms.
  */
 class CdpTaskEntityForm extends ContentEntityForm {
 
@@ -23,12 +23,12 @@ class CdpTaskEntityForm extends ContentEntityForm {
     $logger_arguments = $message_arguments + ['link' => render($link)];
 
     if ($result == SAVED_NEW) {
-      $this->messenger()->addStatus($this->t('New cdp_task_entity %label has been created.', $message_arguments));
-      $this->logger('cdp_task_entity')->notice('Created new cdp_task_entity %label', $logger_arguments);
+      $this->messenger()->addStatus($this->t('New cdp task entity %label has been created.', $message_arguments));
+      $this->logger('cdp_task_entity')->notice('Created new cdp task entity %label', $logger_arguments);
     }
     else {
-      $this->messenger()->addStatus($this->t('The cdp_task_entity %label has been updated.', $message_arguments));
-      $this->logger('cdp_task_entity')->notice('Updated new cdp_task_entity %label.', $logger_arguments);
+      $this->messenger()->addStatus($this->t('The cdp task entity %label has been updated.', $message_arguments));
+      $this->logger('cdp_task_entity')->notice('Updated new cdp task entity %label.', $logger_arguments);
     }
 
     $form_state->setRedirect('entity.cdp_task_entity.canonical', ['cdp_task_entity' => $entity->id()]);
